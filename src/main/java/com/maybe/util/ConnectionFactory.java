b31527f32a7fb8e93c0a7b70eb6beae2700e17e3
Maybe <2542610526@qq.com>
2016-11-01 16:51:21 +0800

    删除无用文件、代码

--- src/main/java/com/maybe/util/ConnectionFactory.java
+++ src/main/java/com/maybe/util/ConnectionFactory.java
@@ -11,18 +11,8 @@ import java.sql.*;
 public class ConnectionFactory {
     protected static Logger logger = Logger.getLogger(ConnectionFactory.class);
 
-    private static final String DBDRIVER = "com.mysql.jdbc.Driver"; // 驱动串
-
-    /* 0.228 - Linux */
-//    private static final String DBURL = "jdbc:mysql://192.168.0.228:3306/orcl?useUnicode=true&characterEncoding=GBK";//
-//    private static final String DBUSER = "bmacsj"; // 用户名
-//    private static final String DBPASSWORD = "sa"; // 数据库密码
-
-//    //    /* 0.253 */
-//    private static final String DBURL = "jdbc:mysql://192.168.0.253:3306/quick4j";//
-//    //private static final String DBURL = "jdbc:mysql://heigoserver-BDA:3306/orcl?useUnicode=true&characterEncoding=GBK";//
-//    private static final String DBUSER = "bmacsj"; // 用户名
-//    private static final String DBPASSWORD = "sa"; // 数据库密码
+    // mysql驱动串
+    private static final String DBDRIVER = "com.mysql.jdbc.Driver";
 
 	/* 本机 */
      private static final String DBURL =
@@ -30,10 +20,6 @@ public class ConnectionFactory {
      private static final String DBUSER = "root"; // 用户名
      private static final String DBPASSWORD = "HX123456"; // 数据库密码
 
-	/* 阿里云 */
-//	 private static final String DBURL = "jdbc:mysql://rdsz84r41i79b9ya6648.mysql.rds.aliyuncs.com:3306/orcl?useUnicode=true&characterEncoding=GBK";//
-//	 private static final String DBUSER = "bmacsj"; // 用户名
-//	 private static final String DBPASSWORD = "123456"; // 数据库密码
 
     /**
      * 打开MySql数据库连接
@@ -108,15 +94,11 @@ public class ConnectionFactory {
      */
     public static Connection getConnectionOracle() {
         try {
-//            Class.forName("oracle.jdbc.driver.OracleDriver");// 加载Oracle驱动程序
-//            String url = "jdbc:oracle:thin:@heigoServer:1521:orcl";// 127.0.0.1是本机地址，XE是精简版Oracle的默认数据库名
-//            String user = "bmacsj"; // 用户名
-//            String password = "sa"; // 数据库密码
 
             Class.forName("oracle.jdbc.driver.OracleDriver");// 加载Oracle驱动程序
             String url = "jdbc:oracle:thin:@192.168.0.241:1521:ORCL";
-            String user = "hgora"; // 用户名
-            String password = "hgORA1234"; // 数据库密码
+            String user = "test"; // 用户名
+            String password = "test"; // 数据库密码
 
             conn = DriverManager.getConnection(url, user, password);// 获取连接
         } catch (Exception e) {
@@ -129,7 +111,7 @@ public class ConnectionFactory {
     public static void main(String[] args) {
         long startDateTime = System.currentTimeMillis();
         System.out.println("Start: " + startDateTime);
-        getConnection();
+        getConnectionOracle();
         long endDateTime = System.currentTimeMillis();
         System.out.println("End: " + endDateTime);
         long result = endDateTime - startDateTime;
